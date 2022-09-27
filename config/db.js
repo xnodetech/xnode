38107a8ba81192e44e033a82b9e39daa1f1f1723
Emmanuel Ezele <emmaezele@yahoo.com>
2022-09-27 01:59:29 +0100

    updated

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 const { MONGO_URI } = process.env
-
+const PORT = process.env.PORT || config.httpPort;
 const connectDB = (app) => {
     mongoose.connect(MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }).then(() => { 
-        app.listen(process.env.PORT || 4000,()=>{console.log(" XNODE APP is running on ")})
+        app.listen(PORT,()=>{console.log(" XNODE APP is running on ")})
 
     }).catch((err) => {
         console.log(err.message)
