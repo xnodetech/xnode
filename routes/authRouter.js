fa8c475ef475034b17dcb0d459c9ce870c15bd2d
Emmanuel Ezele <emmaezele@yahoo.com>
2022-10-15 09:33:03 +0100

    updated

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -41,16 +41,16 @@ const AuthController = require("../controllers/auth");
 /**
  * @swagger
  *  tags:
- *    name: 
+ *    name: Authentication
  *    description: Login AND Signup
  */
-
+ router.post('/signup', AuthController.registerNewUser)
 /**
  * @swagger
  * /auth/signup:
  *   post:
  *     summary: Create a new user
- *     tags: [USER]
+ *     tags: [Authentication]
  *     requestBody:
  *       required: true
  *       content:
@@ -71,9 +71,40 @@ const AuthController = require("../controllers/auth");
  */
 
 
-
-router.post('/signup', AuthController.registerNewUser)
 router.post('/login', AuthController.loginUser)
+/**
+* @swagger
+* /auth/login:
+*   post:
+*     tags: [Authentication]
+*     name: Login
+*     summary: Login a user
+*     consumes:
+*       - application/json
+*     parameters:
+*       - name: body
+*         in: body
+*         schema:
+*           $ref: '#/definitions/User'
+*           type: object
+*           properties:
+*             username:
+*               type: string
+*             password:
+*               type: string
+*               format: password
+*         required:
+*           - username
+*           - password
+*     responses:
+*       200:
+*         description: User found and logged in successfully
+*       401:
+*         description: Incorrect Username or Password
+*       500:
+*         description: Authentication Error
+*/
+
 router.post('/passwordReset', AuthController.resetPassword)
 router.post('/passwordReset/:userId/:token', AuthController.changePassword)
 module.exports = router
